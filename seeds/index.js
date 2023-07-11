3bceec5968507ee90193b9a503b2909837d5b1d1
retnodamayanti <mayahere5@gmail.com>
2023-07-11 14:05:00 +0930

    seeds data

---
+++ seeds/index.js
@@ -0,0 +1,31 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+
+const seedDatabase = async () => {
+  try {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+
+    for (const post of postData) {
+      await Post.create({
+        ...post,
+        userId: users[Math.floor(Math.random() * users.length)].id,
+      });
+    }
+
+    console.log('Seeding completed successfully.');
+    process.exit(0);
+  } catch (err) {
+    console.error('Seeding failed:', err);
+    process.exit(1);
+  }
+};
+
+seedDatabase();
